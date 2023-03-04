d806b950ed29f7f683d1b15bfaf6a4282d36afbc
Darren LaCour <lacourd@icloud.com>
2023-03-03 19:39:58 -0600

    completed site

--- script.js
+++ script.js
@@ -1,19 +1,68 @@
+const affirmations = [
+    "amazing",
+    "beautiful",
+    "intelligent",
+    "smart",
+    "funny",
+    "caring",
+    "creative",
+    "thoughtful",
+    "reflective",
+    "passionate",
+    "compassionate",
+    "fun",
+    "brilliant",
+    "awesome",
+    "incredible",
+    "kind",
+    "warm",
+    "generous",
+    "selfless",
+    "knowledgable",
+    "humble",
+    "polite",
+    "considerate",
+    "principled",
+    "giving",
+    "faithful",
+    "loyal",
+    "a good friend",
+    "a great partner",
+    "one of a kind",
+    "patient",
+    "a good listener",
+    "temperate",
+    "studious",
+    "hard-working",
+    "competent",
+    "courageous",
+    "strong",
+    "persistent",
+    "determined",
+    "sexy",
+    "confident",
+    "gentle"
+];
+
+function randomizer(array) {
+    let randomNumber = Math.floor(Math.random()*array.length)
+    return randomNumber
+};
+
+
 window.addEventListener("load", function(){
-    let json = [];
-    fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
-        response.json().then(function(json) {
-            const destination = document.getElementById("destination");
-            let i=0
-            destination.addEventListener("click", function(){
-                destination.innerHTML = `
+    // const body = document.getElementById("image");
+    const affirmation = document.getElementById("affirmation");
+    window.addEventListener("click", function(){
+        randomNumber = randomizer(affirmations);
+        console.log(randomNumber);
+        affirmation.innerHTML = `
                     <div>
-                    <h3>Planet ${json[i].name}</h3>
-                    <img src=${json[i].image} height=250></img>
+                    <h3 id="affirmationText">...${affirmations[randomNumber].toUpperCase()}!</h3>
                     </div>
                 `;
-                i = (i+1) % json.length;
-            });
-         });
-    });
+        
+    
+        });
 
  });
